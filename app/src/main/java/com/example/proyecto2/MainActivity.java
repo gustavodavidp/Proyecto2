90ac0e521a501eb84d53bd6888928baca2fdd624
Maria Pitti <maria.pitti1@utp.ac.pa>
2021-06-23 19:54:28 -0500

    Nombres

--- app/src/main/java/com/example/proyecto2/MainActivity.java
+++ app/src/main/java/com/example/proyecto2/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.proyecto2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+//Gustavo Polanco 8-943-943 1SF142)
+//Jhonathan Vidal 20-14-4198 1SF142)
+//María Pitti 4-804-1193 1SF142)
+//Juan Torres 8-924-1897 (1SF141)
+//Emir Chacin 8-927-896 1SF142)
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
